a74ce78458dff043edca11dbe4a0e4f0533d4c01
ieunune <une@kakao.com>
2023-02-17 23:55:02 +0900

    [Study] Lec04

---
+++ src/main/java/com/lannstark/lec04/Lec04Main.kt
@@ -0,0 +1,13 @@
+package com.lannstark.lec04
+
+fun main() {
+
+    val money1 = JavaMoney(2_000L);
+    val money2 = JavaMoney(2_000L);
+
+    println(money1.plus(money2))
+
+    // 동일성
+    // 동등성
+
+}
